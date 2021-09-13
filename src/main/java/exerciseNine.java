2103f643176265c3210339c981457e65b43b4789
bedenchw30 <90423350+bedenchw30@users.noreply.github.com>
2021-09-13 18:24:50 -0400

    Initial commit

---
+++ src/main/java/exerciseNine.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class exerciseNine {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in); //scanner object
+
+        System.out.print("Enter Length: ");
+        double Length = input.nextDouble();
+
+        System.out.print("Enter Width: ");
+        double Width = input.nextDouble();
+
+        final double sqrFeetPerGallon = 350;
+        double sqrFeet = Length*Width;
+        int gallon = (int) Math.ceil(sqrFeet/sqrFeetPerGallon); // solves amount of gallons
+
+        System.out.println("You will need to purchase "+ gallon + " gallons to cover " + sqrFeet + " square feet ");
+
+
+    }
+}
